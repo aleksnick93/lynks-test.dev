af1f22968c08adf4a1904ac572eb4c210980a1f6
aleksnick93 <aleksnick93@yandex.ru>
2018-07-21 12:43:41 +0300

    Настройка автозаполнения БД для Item

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Item::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->domainName,
+        'key' => $faker->word
     ];
 });
